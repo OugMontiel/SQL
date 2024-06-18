aae259ad5c16bf23e5fceb148f721df382f82366
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-06-18 09:47:12 -0500

    feat: :construction_worker: Añadi las primeras consultas
    Falta hacer las dinamicas, dinamicas

--- ConectMySql/js/module/partOne.js
+++ ConectMySql/js/module/partOne.js
@@ -1,14 +1,15 @@
 import {connection} from "../../db/connection.js";
 
-// 1. **Recuperar todas las líneas de productos con sus descripciones:**
-export const getAllProducsDescription = async()=>{
-    let [result] = await connection.query("SELECT concat(productLine, textDescription) FROM productlines")
+// 1. Recuperar todas las líneas de productos con sus descripciones:
+export const getAllProducsDescription_1_1 = async()=>{
+    let [result] = await connection.query(
+        `SELECT concat(productLine, textDescription) FROM productlines`
+    )
     return result
 }
 
-// 2. **Encontrar todos los empleados que trabajan en la oficina de 'San Francisco':**
-
-export const getAllEmployeesFromOfficeCode = async ({ officeCode = 1 } = {}) => {
+// 2. Encontrar todos los empleados que trabajan en la oficina de 'San Francisco':
+export const getAllEmployeesFromOfficeCode_1_2 = async ({ officeCode = 1 } = {}) => {
     let [result] = await connection.execute(
         `SELECT * FROM employees WHERE officeCode = ?`,
         [officeCode]
@@ -17,56 +18,68 @@ export const getAllEmployeesFromOfficeCode = async ({ officeCode = 1 } = {}) => 
 }
 
 
-// 3. **Listar todas las órdenes que tienen un estado de 'Enviado':**
+//  3. Listar todas las órdenes que tienen un estado de 'Enviado':
+export const getAllOrdederStatusShipped_1_3 =async()=>{
+    let [result]= await connection.query(
+        `SELECT * FROM orders WHERE status ='Shipped' `
+    );
+    return result;
+}
 
-//    ```sql
-//    select * from orders where status ='Shipped';
-//    ```
+//  4. Obtener los detalles de todos los pagos realizados por el cliente con el número de cliente 103:
+export const getAllPaymentsRealizadosPorCliente_1_4 =async()=>{
+    let [result]= await connection.query(
+        `SELECT * FROM payments WHERE customerNumber=103`
+    );
+    return result;
+}
 
-// 4. **Obtener los detalles de todos los pagos realizados por el cliente con el número de cliente 103:**
+//  5. Recuperar todos los clientes de 'USA' que tienen un límite de crédito superior a 50000:
+export const getAllCustomersUSALimiteCreditoSuperiorA50Mil_1_5 =async()=>{
+    let [result]= await connection.query(
+        `SELECT * FROM customers WHERE country = 'USA' AND creditLimit>50000`
+    );
+    return result;
+}
 
-//    ```sql
-//    select * from payments where customerNumber=103;
-//    ```
+// # Consultas de múltiples tablas
 
-// 5. **Recuperar todos los clientes de 'USA' que tienen un límite de crédito superior a 50000:**
+//  1. Listar todos los productos junto con las descripciones de sus líneas de productos:
+export const getAllProducsUnionDescripcionForProductLine_2_1 =async()=>{
+    let [result]= await connection.query(
+        `SELECT concat(p.productName," ",pl.textDescription) AS "productos junto con las descripciones" FROM products p JOIN productlines pl ON p.productLine = pl.productLine`
+    );
+    return result;
+}
 
-//    ```sql
-//    select * from customers where country = 'USA' and creditLimit>50000;
-//    ```
+//  2. Obtener los nombres y direcciones de correo electrónico de los empleados que reportan al empleado con el número de empleado 1143:
+export const getNameAndEmailFromEmployeesRespondenA_2_2 =async()=>{
+    let [result]= await connection.query(
+        `SELECT lastName, email FROM employees WHERE reportsTo =1143`
+    );
+    return result;
+}
 
-// ### Consultas de múltiples tablas
+//  3. Encontrar todas las órdenes realizadas por clientes de 'Francia':
+export const getAllOrderForCustomerFromCity_2_3 =async()=>{
+    let [result]= await connection.query(
+        `SELECT * FROM orders WHERE customerNumber IN (SELECT customerNumber FROM customers WHERE country = "France")`
+    );
+    return result;
+}
 
-// 1. **Listar todos los productos junto con las descripciones de sus líneas de productos:**
+//  4. Listar el monto total de los pagos recibidos de cada cliente:
+export const getTotalPayForCustomer_2_4 =async()=>{
+    let [result]= await connection.query(
+        `SELECT customerNumber, SUM(amount) AS totalPayments FROM payments GROUP BY customerNumber`
+    );
+    return result;
+}
 
-//    ```sql
-//     SELECT concat(p.productName," ",pl.textDescription) as "productos junto con las descripciones" FROM products p JOIN productlines pl ON p.productLine = pl.productLine;
-   
-//    ```
-
-// 2. **Obtener los nombres y direcciones de correo electrónico de los empleados que reportan al empleado con el número de empleado 1143:**
-
-//    ```sql
-//    select lastName, email from employees where reportsTo =1143;
-//    ```
-
-// 3. **Encontrar todas las órdenes realizadas por clientes de 'Francia':**
-
-//    ```sql
-//    select * from orders where customerNumber In (select customerNumber from customers where country = "France")
-//    ```
-
-// 4. **Listar el monto total de los pagos recibidos de cada cliente:**
-
-//    ```sql
-//    SELECT customerNumber, SUM(amount) AS totalPayments FROM payments GROUP BY customerNumber;
-//    ```
-
-// 5. **Recuperar los detalles de las órdenes, incluyendo los nombres de los productos, para todas las órdenes realizadas por el cliente con el número de cliente 101:**
-
-//    ```sql
-//    SELECT od.orderNumber, p.productName, od.productCode, od.quantityOrdered, od.priceEach
-//    FROM orderdetails od
-//    JOIN products p ON od.productCode = p.productCode
-//    WHERE od.orderNumber IN (SELECT orderNumber FROM orders WHERE customerNumber = 103);
-//    ```
+//  5. Recuperar los detalles de las órdenes, incluyendo los nombres de los productos, para todas las órdenes realizadas por el cliente con el número de cliente 101:
+export const getDetalleOrderForOrderByCustomer =async()=>{
+    let [result]= await connection.query(
+        `SELECT od.orderNumber, p.productName, od.productCode, od.quantityOrdered, od.priceEach FROM orderdetails od JOIN products p ON od.productCode = p.productCode WHERE od.orderNumber IN (SELECT orderNumber FROM orders WHERE customerNumber = 103)`
+    );
+    return result;
+}
