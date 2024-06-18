b1697585c15ba77dc4d41e32b83515c77b99fcd4
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-06-18 17:31:26 -0500

    feat: :sparkles: Parte 2 terminada
    Combres arreglados y verificados

--- ConectMySql/js/main.js
+++ ConectMySql/js/main.js
@@ -1,7 +1,3 @@
-
-//////////////
-// Part One //
-//////////////
 import {
     getAllProducsDescription_1_1,
     getAllEmployeesFromOfficeCode_1_2,
@@ -24,8 +20,32 @@ import {
     getAvgQuantityProductsInOrdersDetails_3_7,
     getBuyPriceOfAllProducts_3_8,
     getAvgSuggestedPriceOfProducts_3_9,
-    getNumberOfEmployeesByJobTitle_3_10
+    getNumberOfEmployeesByJobTitle_3_10,
+    getPaymentsReceivedByClient_4_1,
+    getAvgCreditLimitByCountry_4_2,
+    getTotalOrderByCustomer_4_3,
+    getTotalQuantityOfProductsOrderedByCustome_4_4,
+    getTotalSalesByCustomer_4_5,
+    getAvgStockByProductLine_4_6,
+    getTotalPaymentsByCountry_4_7,
+    getAvgSalesPerEmployee_4_8,
+    getTotalOrdersPerEmployee_4_9,
+    getTotalProductsSoldPerProductLine_4_10,
+    getAvgProductsOrderedPerCustomer_4_11,
+    getTotalSalesPerCountry_4_12,
+    getAvgBuyPricePerProductLine_4_13,
+    getTotalProductsSoldByEmployees_4_14,
+    getTotalPaymentsReceivedByEmployees_4_15,
+    getAverageCreditLimitBySalesperson_4_16,
+    getTotalSalesByOffice_4_17,
+    getAvgProductsOrderedPerCustomer_4_18,
+    getTotalPaymentsPerYear_4_19,
+    getAvgPriceByProductLine_4_20
 } from "./module/partTwo.js";
+
+//////////////
+// Part One //
+//////////////
 
 console.log(await getAllProducsDescription_1_1());
 console.log(await getAllEmployeesFromOfficeCode_1_2({ officeCode: 1 }));
@@ -42,8 +62,6 @@ console.log(await getDetalleOrderForOrderByCustomer_2_5({ codeCustomer: 103 }));
 // Part Two //
 //////////////
 
-
-
 console.log(await getAvgCreditLimit_3_1());
 console.log(await getAllStock_3_2());
 console.log(await getAvgBuyPriceForProducts_3_3());
@@ -54,3 +72,24 @@ console.log(await getAvgQuantityProductsInOrdersDetails_3_7());
 console.log(await getBuyPriceOfAllProducts_3_8());
 console.log(await getAvgSuggestedPriceOfProducts_3_9());
 console.log(await getNumberOfEmployeesByJobTitle_3_10());
+
+console.log(await getPaymentsReceivedByClient_4_1());
+console.log(await getAvgCreditLimitByCountry_4_2());
+console.log(await getTotalOrderByCustomer_4_3());
+console.log(await getTotalQuantityOfProductsOrderedByCustome_4_4());
+console.log(await getTotalSalesByCustomer_4_5());
+console.log(await getAvgStockByProductLine_4_6());
+console.log(await getTotalPaymentsByCountry_4_7());
+console.log(await getAvgSalesPerEmployee_4_8());
+console.log(await getTotalOrdersPerEmployee_4_9());
+console.log(await getTotalProductsSoldPerProductLine_4_10());
+console.log(await getAvgProductsOrderedPerCustomer_4_11());
+console.log(await getTotalSalesPerCountry_4_12());
+console.log(await getAvgBuyPricePerProductLine_4_13());
+console.log(await getTotalProductsSoldByEmployees_4_14());
+console.log(await getTotalPaymentsReceivedByEmployees_4_15());
+console.log(await getAverageCreditLimitBySalesperson_4_16());
+console.log(await getTotalSalesByOffice_4_17());
+console.log(await getAvgProductsOrderedPerCustomer_4_18());
+console.log(await getTotalPaymentsPerYear_4_19());
+console.log(await getAvgPriceByProductLine_4_20());
--- ConectMySql/js/module/partTwo.js
+++ ConectMySql/js/module/partTwo.js
@@ -91,7 +91,7 @@ export const getNumberOfEmployeesByJobTitle_3_10 = async () => {
 
 
 // 1. Calcular el total de pagos recibidos por cada cliente:
-export const get_4_1 = async () => {
+export const getPaymentsReceivedByClient_4_1 = async () => {
     let [result] = await connection.query(
         ` SELECT c.customerName, SUM(p.amount) AS 'total de pagos' FROM customers c INNER JOIN payments p ON p.customerNumber = c.customerNumber GROUP BY p.customerNumber UNION ALL SELECT 'total de pagos recibidos', SUM(amount) FROM payments `
     );
@@ -99,7 +99,7 @@ export const get_4_1 = async () => {
 }
 
 // 2. Obtener el promedio del límite de crédito de los clientes por país:
-export const get_4_2 = async () => {
+export const getAvgCreditLimitByCountry_4_2 = async () => {
     let [result] = await connection.query(
         ` SELECT country, AVG(creditLimit) FROM customers GROUP BY country `
     );
@@ -107,7 +107,7 @@ export const get_4_2 = async () => {
 }
 
 // 3. Calcular el total de órdenes realizadas por cada cliente:
-export const get_4_3 = async () => {
+export const getTotalOrderByCustomer_4_3 = async () => {
     let [result] = await connection.query(
         ` SELECT customerNumber, COUNT(orderNumber) FROM orders GROUP BY customerNumber `
     );
@@ -115,7 +115,7 @@ export const get_4_3 = async () => {
 }
 
 // 4. Encontrar la cantidad total de productos pedidos por cada cliente:
-export const get_4_4 = async () => {
+export const getTotalQuantityOfProductsOrderedByCustome_4_4 = async () => {
     let [result] = await connection.query(
         ` SELECT r.customerNumber, SUM(o.quantityOrdered) AS 'total de productos pedidos' FROM orders r INNER JOIN orderdetails o ON o.orderNumber = r.orderNumber GROUP BY r.customerNumber `
     );
@@ -123,7 +123,7 @@ export const get_4_4 = async () => {
 }
 
 // 5. Calcular el total de ventas (cantidad ordenada por precio cada uno) por cada cliente:
-export const get_4_5 = async () => {
+export const getTotalSalesByCustomer_4_5 = async () => {
     let [result] = await connection.query(
         ` SELECT c.customerName, SUM(p.amount) AS 'total de ventas' FROM customers c INNER JOIN payments p ON c.customerNumber = p.customerNumber GROUP BY c.customerNumber ORDER BY SUM(p.amount) `
     );
@@ -131,7 +131,7 @@ export const get_4_5 = async () => {
 }
 
 // 6. Obtener el promedio de la cantidad de productos en stock por línea de productos:
-export const get_4_6 = async () => {
+export const getAvgStockByProductLine_4_6 = async () => {
     let [result] = await connection.query(
         ` SELECT productLine, AVG(quantityInStock) FROM products GROUP BY productLine `
     );
@@ -139,7 +139,7 @@ export const get_4_6 = async () => {
 }
 
 // 7. Calcular el total de pagos recibidos por cada país:
-export const get_4_7 = async () => {
+export const getTotalPaymentsByCountry_4_7 = async () => {
     let [result] = await connection.query(
         ` SELECT c.country, SUM(p.amount) FROM customers c INNER JOIN payments p GROUP BY c.country `
     );
@@ -147,7 +147,7 @@ export const get_4_7 = async () => {
 }
 
 // 8. Encontrar el promedio de ventas (cantidad ordenada por precio cada uno) por cada empleado:
-export const get_4_8 = async () => {
+export const getAvgSalesPerEmployee_4_8 = async () => {
     let [result] = await connection.query(
         ` SELECT CONCAT(e.firstName, ' ' ,e.lastName) AS 'Full Name employee', AVG(p.amount) AS 'promedio de ventas' FROM employees e INNER JOIN customers c ON c.salesRepEmployeeNumber = e.employeeNumber INNER JOIN payments p ON c.customerNumber = p.customerNumber GROUP BY e.employeeNumber ORDER BY AVG(p.amount) `
     );
@@ -155,7 +155,7 @@ export const get_4_8 = async () => {
 }
 
 // 9. Calcular el total de órdenes gestionadas por cada empleado:
-export const get_4_9 = async () => {
+export const getTotalOrdersPerEmployee_4_9 = async () => {
     let [result] = await connection.query(
         ` SELECT CONCAT(e.firstName, ' ' ,e.lastName) AS 'Full Name employees', COUNT(o.orderNumber) AS 'total de órdenes gestionadas' FROM customers c JOIN orders o ON c.customerNumber = o.customerNumber JOIN employees e ON e.employeeNumber = c.salesRepEmployeeNumber GROUP BY c.salesRepEmployeeNumber `
     );
@@ -163,7 +163,7 @@ export const get_4_9 = async () => {
 }
 
 // 10. Obtener la cantidad total de productos vendidos por cada línea de productos:
-export const get_4_10 = async () => {
+export const getTotalProductsSoldPerProductLine_4_10 = async () => {
     let [result] = await connection.query(
         ` SELECT p.productLine, SUM(o.quantityOrdered) AS 'cantidad total de productos vendidos' FROM products p JOIN orderdetails o USING (productCode) GROUP BY productLine `
     );
@@ -171,7 +171,7 @@ export const get_4_10 = async () => {
 }
 
 // 11. Encontrar el promedio de la cantidad de productos ordenados por cada cliente:
-export const get_4_11 = async () => {
+export const getAvgProductsOrderedPerCustomer_4_11 = async () => {
     let [result] = await connection.query(
         ` SELECT c.customerName, AVG(o.quantityOrdered) AS ' promedio de productos ordenados' FROM customers c JOIN orders USING (customerNumber) JOIN orderdetails o USING  (orderNumber) GROUP BY c.customerNumber `
     );
@@ -179,7 +179,7 @@ export const get_4_11 = async () => {
 }
 
 // 12. Calcular el total de ventas realizadas en cada país:
-export const get_4_12 = async () => {
+export const getTotalSalesPerCountry_4_12 = async () => {
     let [result] = await connection.query(
         ` SELECT c.country, SUM(p.amount) AS 'total de ventas' FROM customers c JOIN payments p USING (customerNumber) GROUP BY c.country ORDER BY SUM(p.amount) `
     );
@@ -187,7 +187,7 @@ export const get_4_12 = async () => {
 }
 
 // 13. Obtener el promedio del precio de compra de los productos por línea de productos:
-export const get_4_13 = async () => {
+export const getAvgBuyPricePerProductLine_4_13 = async () => {
     let [result] = await connection.query(
         ` SELECT productLine, AVG(buyPrice) AS 'promedio del precio de compra' FROM products GROUP BY productLine `
     );
@@ -195,7 +195,7 @@ export const get_4_13 = async () => {
 }
 
 // 14. Encontrar la cantidad total de productos vendidos por cada vendedor:
-export const get_4_14 = async () => {
+export const getTotalProductsSoldByEmployees_4_14 = async () => {
     let [result] = await connection.query(
         ` SELECT CONCAT(e.firstName, ' ' ,e.lastName) AS 'Full Name employees', SUM(o.quantityOrdered) AS 'cantidad total de productos vendidos' FROM employees e JOIN customers ON e.employeeNumber = customers.salesRepEmployeeNumber JOIN orders USING (customerNumber) JOIN orderdetails o USING (orderNumber) GROUP BY e.employeeNumber  `
     );
@@ -203,7 +203,7 @@ export const get_4_14 = async () => {
 }
 
 // 15. Calcular el total de pagos recibidos por cada vendedor:
-export const get_4_15 = async () => {
+export const getTotalPaymentsReceivedByEmployees_4_15 = async () => {
     let [result] = await connection.query(
         ` SELECT CONCAT(e.firstName, ' ', e.lastName) AS 'Full name employees', COUNT(p.customerNumber) AS 'Cuantos pagos', SUM(p.amount) AS 'Total del pago' FROM employees e JOIN customers ON customers.salesRepEmployeeNumber = e.employeeNumber JOIN payments p USING (customerNumber) GROUP BY e.employeeNumber ORDER BY SUM(p.amount), COUNT(p.customerNumber) `
     );
@@ -211,7 +211,7 @@ export const get_4_15 = async () => {
 }
 
 // 16. Obtener el promedio del límite de crédito de los clientes atendidos por cada vendedor:
-export const get_4_16 = async () => {
+export const getAverageCreditLimitBySalesperson_4_16 = async () => {
     let [result] = await connection.query(
         ` SELECT e.employeeNumber, CONCAT(e.firstName, ' ' ,e.lastName) AS 'Full Name employees', AVG(c.creditLimit) AS 'promedio del límite de crédito' FROM employees e JOIN customers c ON e.employeeNumber = c.salesRepEmployeeNumber GROUP BY e.employeeNumber `
     );
@@ -219,7 +219,7 @@ export const get_4_16 = async () => {
 }
 
 // 17. Encontrar el total de ventas realizadas por cada oficina:
-export const get_4_17 = async () => {
+export const getTotalSalesByOffice_4_17 = async () => {
     let [result] = await connection.query(
         ` SELECT o.officeCode, o.city, o.country , SUM(p.amount) as'total de ventas' FROM offices o JOIN employees e USING (officeCode) JOIN customers c ON e.employeeNumber = c.salesRepEmployeeNumber JOIN payments p USING (customerNumber) GROUP BY o.officeCode ORDER BY SUM(p.amount) desc `
     );
@@ -227,7 +227,7 @@ export const get_4_17 = async () => {
 }
 
 // 18. Calcular la cantidad media de productos pedidos por cada cliente:
-export const get_4_18 = async () => {
+export const getAvgProductsOrderedPerCustomer_4_18 = async () => {
     let [result] = await connection.query(
         ` SELECT AVG(o.quantityOrdered) AS 'media de productos pedidos', c.customerName AS 'Customer full Names' FROM customers c JOIN orders USING (customerNumber) JOIN orderdetails o USING (orderNumber) GROUP BY c.customerNumber `
     );
@@ -235,7 +235,7 @@ export const get_4_18 = async () => {
 }
 
 // 19. Obtener el total de pagos realizados en cada año:
-export const get_4_19 = async () => {
+export const getTotalPaymentsPerYear_4_19 = async () => {
     let [result] = await connection.query(
         ` SELECT YEAR(paymentDate) AS años, SUM(amount) AS 'Total de pagos realizados'  FROM payments GROUP BY YEAR(paymentDate) `
     );
@@ -243,7 +243,7 @@ export const get_4_19 = async () => {
 }
 
 // 20. Encontrar el promedio del precio de venta (priceEach) de los productos por línea de productos:
-export const get_4_20 = async () => {
+export const getAvgPriceByProductLine_4_20 = async () => {
     let [result] = await connection.query(
         ` SELECT p.productLine, AVG(o.priceEach) as'promedio del precio de venta' FROM products p JOIN orderdetails o USING (productCode) GROUP BY p.productLine `
     );
