9fc854ef9122bb4aa34af80f0188b5d4bf5c44ee
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-06-23 16:19:05 -0500

    feat: :zap: Examen: termine las tablas
    Modifique algo el readme para lograr hacer un analisas mas profundo

--- ExamenMySQL/tablas.sql
+++ ExamenMySQL/tablas.sql
@@ -1,75 +1,118 @@
-Table empleados {
-    id int [pk, increment]
-    nombre varchar(50)
-    telefono varchar(20)
-    nif varchar(20)
-    numeroSeguridadSocial varchar(20)
-    numeroolegiado varchar(20)
-    iddireccion int
-    idTipoEmpleado int
-    idTipoDeContrato int
-}
+CREATE TABLE Empleados (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  nombre varchar(50),
+  telefono varchar(20),
+  nif varchar(20),
+  numero_seguridad_social varchar(20),
+  numero_colegiado varchar(20),
+  idPoblacion int,
+  idDireccion int,
+  idTipoEmpleado int,
+  idTipoDeContrato int
+);
 
-Table direccion {
-    id int [pk, increment]
-    direccion varchar(100)
-    poblacion varchar(50)
-    provincia varchar(50)
-    codigoPostal varchar(10)
-}
+CREATE TABLE Direccion (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  direccion varchar(100),
+  idProvincia int,
+  idCodigoPostal int
+);
 
-Table TipoEmpleado {
-    id int [pk, increment]
-    tipo varchar(50)
-}
-Table TipoDeContrato {
-    id int [pk, increment]
-    tipo varchar(50)
-}
+CREATE TABLE Poblacion (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  nombre varchar(50)
+);
 
-Table horario {
-    id int [pk, increment]
-    idEmpleado int
-    horaInicio time
-    horaFin time
-}
+CREATE TABLE Provincia (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  nombre varchar(50)
+);
 
-Table Reemplazos {
-    id int [pk, increment]
-    idEmpleado int
-    fechaAlta date
-    fechaBaja date
-}
+CREATE TABLE CodigoPostal (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  codigo varchar(10)
+);
 
-Table vacacion {
-    id int [pk, increment]
-    idEmpleado int
-    fechaInicio date
-    fechaFin date
-}
+CREATE TABLE TipoEmpleado (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  tipo_de_empleado varchar(50)
+);
 
-Table paciente {
-    id int [pk, increment]
-    nombre varchar(50)
-    telefono varchar(20)
-    nif varchar(20)
-    numeroSeguridadSocial varchar(20)
-    iddireccion int
-}
+CREATE TABLE TipoDeContrato (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  TipoDeContrato varchar(50)
+);
 
-Table relacionMedicoPaciente {
-    id int [pk, increment]
-    idPaciente int
-    idMedico int
-}
+CREATE TABLE Horario (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  idEmpleado int,
+  idDia int,
+  hora_inicio time,
+  hora_fin time
+);
 
+CREATE TABLE Dias (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  dias varchar(20)
+);
 
-Ref: "Horario"."idEmpleado" > "empleados"."id"
-Ref: "Reemplazos"."idEmpleado" > "empleados"."id"
-Ref: "Vacacion"."idEmpleado" > "empleados"."id"
-Ref: "Paciente"."iddireccion" > "direccion"."id"
-Ref: "RelacionMedicoPaciente"."idPaciente" > "Paciente"."id"
-Ref: "RelacionMedicoPaciente"."idMeSco" > "eSleados"."id"
-Ref: "empleados"."iddireccion" > "DeSion"."idSRef: "empleados"."idTipoEmpleado" > SipoEmplo"."id"
+CREATE TABLE Reemplazos (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  idEmpleado int,
+  idHorario int,
+  fecha_alta_inicio date,
+  fecha_baja_fin date
+);
 
-Ref: "empleados"."idTipoDeContrato" < "TipoDeContra"."id"SS
+CREATE TABLE Vacacion (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  idEmpleado int,
+  fecha_inicio date,
+  fecha_fin date
+);
+
+CREATE TABLE Paciente (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  nombre varchar(50),
+  telefono varchar(20),
+  nif varchar(20),
+  numero_seguridad_social varchar(20),
+  idDireccion int,
+  idPoblacion int
+);
+
+CREATE TABLE RelacionMedicoPaciente (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  idPaciente int,
+  idMedico int
+);
+
+ALTER TABLE Empleados ADD FOREIGN KEY (idPoblacion) REFERENCES Poblacion (id);
+
+ALTER TABLE Empleados ADD FOREIGN KEY (idDireccion) REFERENCES Direccion (id);
+
+ALTER TABLE Empleados ADD FOREIGN KEY (idTipoEmpleado) REFERENCES TipoEmpleado (id);
+
+ALTER TABLE Empleados ADD FOREIGN KEY (idTipoDeContrato) REFERENCES TipoDeContrato (id);
+
+ALTER TABLE Direccion ADD FOREIGN KEY (idProvincia) REFERENCES Provincia (id);
+
+ALTER TABLE Direccion ADD FOREIGN KEY (idCodigoPostal) REFERENCES CodigoPostal (id);
+
+ALTER TABLE Horario ADD FOREIGN KEY (idEmpleado) REFERENCES Empleados (id);
+
+ALTER TABLE Horario ADD FOREIGN KEY (idDia) REFERENCES Dias (id);
+
+ALTER TABLE Reemplazos ADD FOREIGN KEY (idEmpleado) REFERENCES Empleados (id);
+
+ALTER TABLE Reemplazos ADD FOREIGN KEY (idHorario) REFERENCES Horario (id);
+
+ALTER TABLE Vacacion ADD FOREIGN KEY (idEmpleado) REFERENCES Empleados (id);
+
+ALTER TABLE Paciente ADD FOREIGN KEY (idDireccion) REFERENCES Direccion (id);
+
+ALTER TABLE Paciente ADD FOREIGN KEY (idPoblacion) REFERENCES Poblacion (id);
+
+ALTER TABLE RelacionMedicoPaciente ADD FOREIGN KEY (idPaciente) REFERENCES Paciente (id);
+
+ALTER TABLE RelacionMedicoPaciente ADD FOREIGN KEY (idMedico) REFERENCES Empleados (id);
